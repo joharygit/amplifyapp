249e83c2f1526aab95366ac912d67deeac6efcc0
Johary Ramahatra <joharyramahatra@gmail.com>
2022-02-14 18:44:56 +0100

    use Authenticator with ui-react v2 StackOverflow was helpful

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react-v1';
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
-function App() {
+
+const  App = () =>  {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1> We now have Auth</h1>
-        <h2> Hello from V3 .nametraka Auth</h2>
-        
-      </header>
-      <AmplifySignOut/>
-    </div>
+      <Authenticator>
+        {
+          ( {signOut, user }) => (
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1> We now have Auth</h1>
+              <h2> Hello from V3 .nametraka Auth</h2>
+              <button onClick={signOut}>Sign out</button>
+            </header>
+      
+          </div>
+        )
+
+      }
+      
+     </Authenticator> 
   );
 }
 
-export default withAuthenticator(App);
+export default App;
