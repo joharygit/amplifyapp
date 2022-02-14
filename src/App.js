2487c73a5fdaafe5337dfbc485e5330bd51401d7
Johary Ramahatra <joharyramahatra@gmail.com>
2022-02-14 17:37:53 +0100

    added auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react-v1';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1> Hello from V2 .demarrage du projet ry mama</h1>
+        <h1> We now have Auth</h1>
+        <h2> Hello from V3 .nametraka Auth</h2>
         
       </header>
+      <AmplifySignOut/>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
